b258cdb4f106cd4664c659ef677f9cc3783c2a6a
rxliuli <rxliuli@gmail.com>
2021-08-21 14:44:00 +0800

    feat: 实现关于页面

--- src/views/about/AboutPage.tsx
+++ src/views/about/AboutPage.tsx
@@ -1,5 +1,56 @@
 import * as React from 'react'
+import ReactMarkdown from 'react-markdown'
+import css from './AboutPage.module.css'
+
+const Icon: React.FC<{
+  href: string
+}> = (props) => (
+  <a href={props.href} target={'_blank'} className={css.icon}>
+    {props.children}
+  </a>
+)
+
+function Banner() {
+  return (
+    <div className={css.banner}>
+      <div className={css.mask}>
+        <section className={css.title}>
+          <h2>关于吾辈</h2>
+          <p>
+            吾辈是 rxliuli（中文名是
+            琉璃），喜欢现代前端的全沾开发者（曾经的后端开发）
+          </p>
+          <Space>
+            <Icon href={'https://github.com/rxliuli'}>
+              <i className="fab fa-github fa-2x" />
+            </Icon>
+            <Icon href={'https://blog.rxliuli.com/'}>
+              <i className="fas fa-blog fa-2x" />
+            </Icon>
+            <Icon href={'https://twitter.com/rxliuli'}>
+              <i className="fab fa-twitter fa-2x" />
+            </Icon>
+            <Icon href={'https://t.me/rxliuli'}>
+              <i className="fab fa-telegram fa-2x" />
+            </Icon>
+            <Icon href={'mailto:rxliuli@gmail.com'}>
+              <i className="fas fa-envelope-square fa-2x" />
+            </Icon>
+          </Space>
+        </section>
+      </div>
+    </div>
+  )
+}
+
+import aboutContent from './about.md?raw'
+import { Space } from '../../components/Space'
 
 export const AboutPage: React.FC = () => {
-  return <div>关于页面</div>
+  return (
+    <div>
+      <Banner />
+      <ReactMarkdown className={css.content}>{aboutContent}</ReactMarkdown>
+    </div>
+  )
 }
---
+++ src/views/about/about.md
@@ -0,0 +1,10 @@
+## rxliuli（琉璃）👨‍💻
+
+- **工作 :** Web 前端开发
+- **学习 :** Full-Stack ⚡ | Open-Source 🔥
+- **兴趣 :** Books 📚 预备 Kiger 😄
+- **帮助 :** 如果遇到任何 `TypeScript/Jetbrains IDE/Windows 工具` 的问题可以问吾辈！✌️
+- **代称 :** rxliuli/琉璃 😇
+
+![github stats](https://github-readme-stats.vercel.app/api?username=rxliuli&show_icons=true&theme=tokyonight&line_height=40&v=5)
+![github used languages](https://github-readme-stats.vercel.app/api/top-langs/?username=rxliuli&theme=tokyonight&hide=python,shell)
