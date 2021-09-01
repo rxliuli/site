b7f218cb9ae4e5e28a5a2fd027794fa277095673
rxliuli <rxliuli@gmail.com>
2021-09-01 22:55:48 +0800

    chore: 使用 hash 路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import 'normalize.css'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { HashRouter, Route, Switch } from 'react-router-dom'
 import { Header } from './views/home/component/Header'
 import HomePage from './views/home'
 import AboutPage from './views/about'
@@ -8,7 +8,7 @@ import { NotMatchPage } from './views/404'
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <Switch>
         <Route path={'/'} exact={true}>
@@ -21,7 +21,7 @@ const App: React.FC = () => {
           <NotMatchPage />
         </Route>
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
