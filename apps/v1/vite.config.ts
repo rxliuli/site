6011bfca1dc71de1332cc4bb8549e3e4cd6b56e2
rxliuli <rxliuli@gmail.com>
2022-03-16 00:02:24 +0800

    chore: 添加插件分析 react 尺寸

--- apps/v1/vite.config.ts
+++ apps/v1/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import { visualizer } from 'rollup-plugin-visualizer'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [reactRefresh(), visualizer()],
 })
