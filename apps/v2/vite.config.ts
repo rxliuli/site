0d12cad252fee074acb3fdc37b56a4b41c420b41
rxliuli <rxliuli@gmail.com>
2022-03-23 21:59:59 +0800

    fix: 修复构建配置错误

--- apps/v2/vite.config.ts
+++ apps/v2/vite.config.ts
@@ -3,6 +3,5 @@ import preact from '@preact/preset-vite'
 import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
-  base: '/v2/',
   plugins: [preact(), visualizer()],
 })
