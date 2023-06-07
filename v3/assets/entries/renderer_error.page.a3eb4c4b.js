bfd411844f951ba0d9be1be0def5ae46e45f8833
rxliuli <rxliuli@gmail.com>
2023-06-07 13:19:27 +0800

    Updates

---
+++ v3/assets/entries/renderer_error.page.a3eb4c4b.js
@@ -0,0 +1,1 @@
+import{a as n,F as r,j as e}from"../chunks/chunk-0e184ced.js";function t({is404:o}){return o?n(r,{children:[e("h1",{children:"404 Page Not Found"}),e("p",{children:"This page could not be found."})]}):n(r,{children:[e("h1",{children:"500 Internal Error"}),e("p",{children:"Something went wrong."})]})}export{t as Page};
