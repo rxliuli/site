cfe26db29c6e29b65d16cf07c8649b7b162e2a77
rxliuli <rxliuli@gmail.com>
2024-09-15 22:54:38 +0800

    feat: 支持 ssg

--- packages/v4/src/routes/+layout.svelte
+++ packages/v4/src/routes/+layout.svelte
@@ -4,12 +4,7 @@
   import { i18n } from '$lib/i18n/store'
   import { browser } from '$app/environment'
 
-  export let data: {
-    locale: string
-  }
-
-  // Use the locale from the server if available, otherwise fallback to browser language
-  const locale = data.locale ?? (browser ? navigator.language : 'en-US')
+  const locale = browser ? navigator.language : 'en-US'
   i18n.setLocale(locale.includes('zh') ? 'zh-CN' : 'en-US')
 </script>
 
